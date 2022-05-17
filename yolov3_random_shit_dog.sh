3e137694823e0bc075dcd357f539409fba7b1c39
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2022-05-17 15:25:53 -0400

    remove extra files

--- yolov3_random_shit_dog.sh
+++
@@ -1,2 +0,0 @@
-cd models/detectors/yolov3
-python3 train.py --img 416 --batch 16 --epochs 350 --data oxford_pet.yaml --weights '' --cfg models/yolov3-msa.yaml --adam --hyp data/hyps/hyp.scratch-msa.yaml
